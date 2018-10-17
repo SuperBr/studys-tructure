3897f30a0eac23c574efe449209d96653c2fac67
Suberb <subetb@163.com>
2018-10-17 18:36:36 +0800

    搜索时

--- src/main/java/com/tree/Node.java
+++ src/main/java/com/tree/Node.java
@@ -19,13 +19,13 @@ public class Node<T, V> extends Entry<T,V> {
         super(null, null);
     }
 
-    public Integer height;
+    private Integer height;
 
-    public Node left;
+    public Node<T, V> left;
 
-    public Node right;
+    public Node<T, V> right;
 
-    public Node parent;
+    public Node<T, V> parent;
 
     public Boolean hasLeft() {
         return left!=null;
@@ -35,6 +35,17 @@ public class Node<T, V> extends Entry<T,V> {
         return right!=null;
     }
 
+    public Integer getHeight() {
+        if (height == null) {
+            return 0;
+        }
+        return height;
+    }
+
+    public Node setHeight(Integer height) {
+        this.height = height;
+        return this;
+    }
 }
 
  class Entry<K, V> {
--- src/main/java/com/tree/balance2Tree/AVLTree.java
+++ src/main/java/com/tree/balance2Tree/AVLTree.java
@@ -27,43 +27,122 @@ public class AVLTree implements AbsTree<Integer, String> {
             return node.value;
         }
 
-        return insert(node).value;
+        return insert(root, node).value;
     }
 
 
     private Node<Integer, String> insert(Node<Integer, String> parent, Node<Integer, String> node) {
         if (parent.key == node.key) {
             parent.value = node.value;
-        }
-        if (parent.key > node.key) {
+        } else if (parent.key > node.key) {
             if (parent.hasLeft()) {
                 insert(parent.left, node);
+                if (Math.abs(getMaxH(parent.left) - getMaxH(parent.right)) >= 2) {
+                    if (parent.left.key < node.key) {
+                        LR(parent);
+                    } else {
+                        LL(parent.left);
+                    }
+
+                }
             } else {
                 parent.left = node;
                 node.parent = parent.left;
-                node.height = parent.height + 1;
+                node.setHeight(parent.getHeight() + 1);
             }
         } else {
             if (parent.hasRight()) {
                 insert(parent.right, node);
+                if (Math.abs(getMaxH(parent.left) - getMaxH(parent.right)) >= 2) {
+                    if (parent.right.key > node.key) {
+                        RL(parent);
+                    } else {
+                        RR(parent);
+                    }
+
+                }
             } else {
                 parent.right = node;
                 node.parent = parent.right;
+                node.setHeight(parent.getHeight() + 1);
             }
         }
         return node;
     }
 
+    //左旋转
+    private void LL(Node<Integer, String> k2) {
+        Node<Integer, String> k1 = k2.parent;
+        k1.left = k2.right;
+        k2.right = k1;
+        k2.parent = k1.parent;
+        k1.parent = k2;
+
+    }
+
+    //右旋转
+    private void RR(Node<Integer, String> k2) {
+        Node<Integer, String> k1 = k2.parent;
+        k2.left = k1;
+        k1.right = k2.left;
+        k1.parent = k2;
+        k2.parent = k1.parent;
+
+    }
+
+    //左右旋转
+    private void LR(Node<Integer, String> k2) {
+        Node<Integer, String> k1 = k2.parent;
+        Node<Integer, String> k3 = k2.right;
+        //1.变为LL
+        k1.left = k3;
+        k3.parent = k1;
+
+        k3.left = k2;
+        k2.parent = k3;
+
+        k2.right = k3.left;
+        k3.left.parent = k3;
+
+        //2. LL
+        LL(k3);
+
+    }
+
+    //右左旋转
+    private void RL(Node<Integer, String> k2) {
+        Node<Integer, String> k1 = k2.parent;
+        Node<Integer, String> k3 = k2.left;
+        //1.变为RR
+        k1.left = k3;
+        k3.parent = k1;
+
+
+        k3.right = k2;
+        k2.parent = k3;
+
+
+        k3.right = k2.left;
+        k2.left.parent = k2;
+
+        RR(k3);
+
+
+    }
+
     //树的高度
     private Integer getMaxH(Node<Integer, String> avlNode) {
-        int h = avlNode.height;
+        if (avlNode == null) {
+            return 0;
+        }
+        int h = avlNode.getHeight();
         if (avlNode.hasLeft()) {
             h = getMaxH(avlNode.left);
         }
         if (avlNode.hasRight()) {
             h = getMaxH(avlNode.right);
         }
-        return h - avlNode.height;
+        return h - avlNode.getHeight();
     }
 
     public String find(Integer integer) {
@@ -72,6 +151,22 @@ public class AVLTree implements AbsTree<Integer, String> {
 
     public Boolean delete(Integer integer) {
         return null;
+    }
+
+
+
+
+    public static void main(String[] args){
+
+        AVLTree avlTree = new AVLTree(28, "28");
+        avlTree.insert(26, "26");
+        avlTree.insert(20, "20");
+        avlTree.insert(48, "48");
+        avlTree.insert(2, "2");
+        avlTree.insert(17, "17");
+        avlTree.insert(18, "18");
+        avlTree.insert(19, "19");
+        avlTree.insert(20, "20");
     }
 
 
